31fb479867eda766b9f8358d1c78ef1e135820bc
Divakar999 <avatadedivakar@gmail.com>
2022-05-17 10:59:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-swazth
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-swazth)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import ControlledForm from './ControlledForms';
+export default function App() {
+  return (
+    <div>
+      <ControlledForm /> 
+    </div>
+  );
+}
